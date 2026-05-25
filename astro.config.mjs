fa41cd49e8245cdc8da02cc104e6e9607996883a
KAHMOOSHA <brhmhelou@gmail.com>
2026-05-25 03:39:15 +0300

    fix astro github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
     site: 'https://KAHMOOSHA.github.io',
     base: '/Anqa---First-Draft/',
-    integrations: [githubPages()],
+
 });
