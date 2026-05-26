1869a5899b7e5ca0b70c3b2aee89aa85ffe1f805
KAHMOOSHA <brhmhelou@gmail.com>
2026-05-26 07:54:43 +0300

    following hunch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+
 
 export default defineConfig({
-    output: 'static',
-    adapter: vercel(),
+    site: 'https://KAHMOOSHA.github.io',
+    base: '/Anqa---First-Draft/',
 });
